2af6bb14ca24d56b8ffa2aa3a2df4c6baf06389d
Jeff Olen <jeff.olen@bethesda.net>
2020-01-12 13:47:42 -0800

    Return swing lows and swing highs to calling program

--- lib/signal_tools/technicals/swing.rb
+++ lib/signal_tools/technicals/swing.rb
@@ -6,7 +6,7 @@ module SignalTools::Technicals
 
     DEFAULT_SWING_DAYS = 3
 
-    attr_reader :data
+    attr_reader :data, :swing_lows, :swing_highs
 
     def initialize(data)
       @data = data
@@ -17,24 +17,20 @@ module SignalTools::Technicals
     end
 
     def find_swings(swing_days=DEFAULT_SWING_DAYS)
-      @swing_highs = find_swing_highs(swing_days)
-      @swing_lows = find_swing_lows(swing_days)
+      find_swing_highs(swing_days)
+      find_swing_lows(swing_days)
       return @swing_lows, @swing_highs
     end
 
     def find_swing_highs(swing_days=DEFAULT_SWING_DAYS)
       @data.dates.slice(3..-2).each_with_index do |dt, idx|
         @swing_highs[idx+3] = @data.close_prices[idx+2] > @data.close_prices[idx+1] && @data.close_prices[idx+2] > @data.close_prices[idx] && @data.close_prices[idx+3] < @data.close_prices[idx+2]
-        #puts "Swing High Date: #{dt}\tindex: #{idx}" if @swing_highs[idx+3] == true
-        #puts "\n\t#{@data.dates[idx]}: #{@data.close_prices[idx]} \n\t#{@data.dates[idx+1]}: #{@data.close_prices[idx+1]} \n\t#{@data.dates[idx+2]}: #{@data.close_prices[idx+2]} \n\t#{@data.dates[idx+3]}: #{@data.close_prices[idx+3]}" if @swing_highs[idx+3] == true
       end
     end
 
     def find_swing_lows(swing_days=DEFAULT_SWING_DAYS)
       @data.dates.slice(2..-2).each_with_index do |dt, idx|
         @swing_lows[idx+3] = @data.close_prices[idx+2] < @data.close_prices[idx+1] && @data.close_prices[idx+2] < @data.close_prices[idx] && @data.close_prices[idx+3] > @data.close_prices[idx+2]
-        #puts "Swing Low Date: #{dt}\tindex: #{idx}" if @swing_lows[idx+3] == true
-        #puts "\n\t#{@data.dates[idx]}: #{@data.close_prices[idx]} \n\t#{@data.dates[idx+1]}: #{@data.close_prices[idx+1]} \n\t#{@data.dates[idx+2]}: #{@data.close_prices[idx+2]} \n\t#{@data.dates[idx+3]}: #{@data.close_prices[idx+3]}" if @swing_lows[idx+3] == true
       end
     end
 
